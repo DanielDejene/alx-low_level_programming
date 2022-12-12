99674415bce59962aab136ed3c596dd8db9c3a8d
DanielDejene <daniel.dejene54@gmail.com>
2022-12-12 09:56:01 +0300

    mj

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
