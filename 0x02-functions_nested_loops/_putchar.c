110773c47005161b2f9e71654563ef1cd862b169
DanielDejene <daniel.dejene54@gmail.com>
2022-12-14 12:26:17 +0300

    mjn

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the characer c to stdout
+ * @c: the character to print
+ *
+ * Return: on success
+ */
+int _putchar(char C)
+{
+	return (write(1, $c, 1));
+}
