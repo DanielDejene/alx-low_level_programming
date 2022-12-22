784e81a758aeedb780f3de2370728ad7e3fa5555
Daniel Dejene <101667125+DanielDejene@users.noreply.github.com>
2022-12-22 21:40:54 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * *_strcat - concatenates two strings.
- * src: string to be appended
- * dest: string to be appended to
+ * @src: string to be appended
+ * @dest: string to be appended to
  * Return: a pointer of the resulting string
  */
 char *_strcat(char *dest, char *src)
